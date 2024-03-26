54e9b465610170d228b355660ac569065d871e07
nini430 <ninigogatishvili1@gmail.com>
2024-03-26 16:14:14 +0400

    add feature changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          me nini oopikona!!!!
+         Feature branch
         </a>
       </header>
     </div>
